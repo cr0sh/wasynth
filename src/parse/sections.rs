a63061ff15fea48469c4e5d16b61d1f9579359d2
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-02-12 10:00:54 +0900

    Make section modules not exposed public

--- src/parse/sections.rs
+++ src/parse/sections.rs
@@ -1,16 +1,16 @@
-pub mod code;
-pub mod custom;
-pub mod data;
-pub mod data_count;
-pub mod element;
-pub mod export;
-pub mod function;
-pub mod global;
-pub mod import;
-pub mod memory;
-pub mod start;
-pub mod table;
-pub mod r#type;
+mod code;
+mod custom;
+mod data;
+mod data_count;
+mod element;
+mod export;
+mod function;
+mod global;
+mod import;
+mod memory;
+mod start;
+mod table;
+mod r#type;
 
 pub use {
     code::*, custom::*, data::*, data_count::*, element::*, export::*, function::*, global::*,

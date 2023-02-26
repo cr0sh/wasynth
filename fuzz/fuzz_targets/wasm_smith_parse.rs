39b903ffc565c730d72688819304160552f0d077
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-02-26 11:02:55 +0900

    Add env_logger to fuzz targets

--- fuzz/fuzz_targets/wasm_smith_parse.rs
+++ fuzz/fuzz_targets/wasm_smith_parse.rs
@@ -1,9 +1,15 @@
 #![no_main]
 
+use std::sync::Once;
+
 use libfuzzer_sys::fuzz_target;
 use wasm_smith::Module;
 
+static INIT_ONCE: Once = Once::new();
+
 fuzz_target!(|module: Module| {
+    INIT_ONCE.call_once(env_logger::init);
+
     let wasm_bytes = module.to_bytes();
 
     let module = wasynth::parse::Module::from_binary(&wasm_bytes).expect("cannot parse module");

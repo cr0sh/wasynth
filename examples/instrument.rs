23a5c436efaa656ebccf4e5480258da86b980295
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2023-03-26 22:54:32 +0900

    Add instrumentation example

---
+++ examples/instrument.rs
@@ -0,0 +1,21 @@
+use difference::Changeset;
+
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    env_logger::init();
+
+    let s = include_bytes!("../tests/cases/wasynth_release.wasm");
+    let wasm = wat::parse_bytes(s)?;
+    let before = wasmprinter::print_bytes(wasm.clone()).unwrap();
+    let mut module = wasynth::parse::Module::from_binary(&wasm)?.into_synth()?;
+    wasynth::instrument::install_all(&mut module)?;
+
+    let mut buf = Vec::new();
+    module.write_into(&mut buf)?;
+
+    let after = wasmprinter::print_bytes(&buf)?;
+
+    let diff = Changeset::new(&before, &after, "\n");
+    eprintln!("{diff}");
+
+    Ok(())
+}

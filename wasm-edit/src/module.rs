afb8430854bb90e8a27f051c241291630768fe64
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-06-18 12:42:57 +0900

    Add docs

--- wasm-edit/src/module.rs
+++ wasm-edit/src/module.rs
@@ -14,30 +14,37 @@ pub struct Module<'a> {
 }
 
 impl<'a> Module<'a> {
+    /// Add a [`FuncType`] value to this Module. Returns the reference to the added value.
     pub fn add_type(&self, ty: FuncType) -> IndexedCell<'_, FuncType> {
         self.context.add_type(ty)
     }
 
+    /// Add a [`Function`] value to this Module. Returns the reference to the added value.
     pub fn add_function(&self, function: Function) -> IndexedCell<'_, Function> {
         self.context.add_function(function)
     }
 
+    /// Add a [`TableType`] value to this Module. Returns the reference to the added value.
     pub fn add_table(&self, table: TableType) -> IndexedCell<'_, TableType> {
         self.context.add_table(table)
     }
 
+    /// Add a [`MemType`] value to this Module. Returns the reference to the added value.
     pub fn add_memory(&self, memory: MemType) -> IndexedCell<'_, MemType> {
         self.context.add_memory(memory)
     }
 
+    /// Add a [`Global`] value to this Module. Returns the reference to the added value.
     pub fn add_global(&self, global: Global) -> IndexedCell<'_, Global> {
         self.context.add_global(global)
     }
 
+    /// Add an [`Element`] value to this Module. Returns the reference to the added value.
     pub fn add_element(&self, element: Element) -> IndexedCell<'_, Element> {
         self.context.add_element(element)
     }
 
+    /// Add a [`Data`] value to this Module. Returns the reference to the added value.
     pub fn add_data(&self, data: Data) -> IndexedCell<'_, Data> {
         self.context.add_data(data)
     }
